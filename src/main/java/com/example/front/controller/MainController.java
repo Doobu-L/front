4ce9584a484071b40ecb1f0ee82638f969258d9e
DESKTOP-SA0VI34\Seop <point455@bankq.co.kr>
2022-07-22 14:18:41 +0900

    attribute 분류해서 화면으로 넘기는 작업

--- src/main/java/com/example/front/controller/MainController.java
+++ src/main/java/com/example/front/controller/MainController.java
@@ -1,7 +1,12 @@
 package com.example.front.controller;
 
+import com.example.front.domain.ScheduleDto;
+import com.example.front.domain.SchedulerDto;
 import com.example.front.domain.UserDto;
 import com.example.front.service.MainService;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -18,7 +23,15 @@ public class MainController {
   @GetMapping
   public String loadMain(ModelMap modelMap){
     UserDto user = mainService.getUser();
+    List<SchedulerDto> schedulerDtos = user.getSchedulerDtos().stream().collect(Collectors.toList());
+    List<ScheduleDto> scheduleDtos = schedulerDtos.stream()
+        .map(SchedulerDto::getSchedules)
+        .flatMap(Collection::stream)
+        .collect(Collectors.toList());
+
     modelMap.addAttribute("user",user);
+    modelMap.addAttribute("schedulers",schedulerDtos);
+    modelMap.addAttribute("schedules",scheduleDtos);
     return "main.html";
   }
 
